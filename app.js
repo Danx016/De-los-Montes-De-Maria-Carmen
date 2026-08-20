e7115a617e60e974d1e10054a261bf9eaa764691
Danx016 <danilorodelo355@gmail.com>
2026-08-20 00:36:27 -0500

    chore: depuracion y eliminacion de archivos obsoletos (sqlite, vistas legacy ejs y scripts sin uso)

--- app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Entrada principal / Proxy de arranque
- * Redirige la ejecución a la Arquitectura Hexagonal en src/framework/server.js
- */
-const path = require('path');
-const server = require(path.join(__dirname, 'src', 'framework', 'server.js'));
-
-module.exports = server;
